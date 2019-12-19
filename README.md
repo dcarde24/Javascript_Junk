4331db2844ba20d83ea4f66dab7951740882810f
David Cardenas <davidcardenas1993@yahoo.com>
2019-12-19 16:49:20 -0700

    added some sample javascript

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Javascript_Junk
+
+In this repository, I will practice using more es6 syntax throughout all code.
---
+++ app.js
@@ -0,0 +1,18 @@
+const doughnut = [
+    apple = "delicious",
+    bearClaw = "wonderful",
+    chocolate = "yumm",
+]
+
+console.log(doughnut);
+
+// let a = 10;
+
+// let b = 80; // this does the same thing as below.
+
+let a, b, sum;
+a = 10;
+b = 80;
+sum = a + b;
+
+console.log(sum);
